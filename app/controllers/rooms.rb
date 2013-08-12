9c0b5ebb3289c8ff9cc9bc21e869e7e6252eae8f
phelrine <phelrine@gmail.com>
2013-08-12 15:41:22 +0900

    Add enter room

--- app/controllers/rooms.rb
+++ app/controllers/rooms.rb
@@ -25,6 +25,14 @@ Server::App.controllers :rooms do
     @room.to_json
   end
 
+  post :enter, :provides => :json do
+    room = Room.find_by_id(params[:room_id])
+    return unless room
+    user = User.find_by_id_and_token(params[:user_id], params[:token])
+    return unless user
+    user.enter_room(room)
+  end
+
   get :list, :provides => :json do
     Room.order('created_at desc').limit(20).to_json
   end
