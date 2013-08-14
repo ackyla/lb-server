627ced5d6c7f5868c416b67332e26d058b420b19
phelrine <phelrine@gmail.com>
2013-08-14 16:28:12 +0900

    JSON        .

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -1,9 +1,11 @@
 Server::App.controllers :locations do
   post :create, :provides => :json do
     login(params)
-    Location.new(:latitude => params[:latitude], :longitude => params[:longitude]){|l|
+    loc = Location.new(:latitude => params[:latitude], :longitude => params[:longitude]){|l|
       l.user = user
       l.room = user.room
-    }.save.to_json
+    }
+    loc.save
+    loc.to_json
   end
 end
