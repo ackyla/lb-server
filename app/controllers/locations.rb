31dbc055c4879a7e0b7b759db7d0dffe56630c36
phelrine <phelrine@gmail.com>
2013-08-14 16:42:35 +0900

    バグは直した

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -2,8 +2,8 @@ Server::App.controllers :locations do
   post :create, :provides => :json do
     login(params)
     loc = Location.new(:latitude => params[:latitude], :longitude => params[:longitude]){|l|
-      l.user = user
-      l.room = user.room
+      l.user = @user
+      l.room = @user.room
     }
     loc.save
     loc.to_json
