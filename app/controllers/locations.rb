9c1cd061d9832078f9b9f7df8b12c4ff975bf5e8
phelrine <phelrine@gmail.com>
2013-08-15 13:58:36 +0900

    Location List cache

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -1,6 +1,7 @@
 Server::App.controllers :locations do
   post :create, :provides => :json do
     login(params)
+    error_message(300, "Cannot post location to inactive room.") unless @user.room.active
     loc = Location.new(:latitude => params[:latitude], :longitude => params[:longitude]){|l|
       l.user = @user
       l.room = @user.room
@@ -8,9 +9,10 @@ Server::App.controllers :locations do
     loc.save
     loc.to_json
   end
-  
-  get :list, :provides => :json do
+
+  get :list, :provides => :json, :cache => true do
+    expires_in 60
     find_room(params)
-    @room.locations.to_json(:include => [:user])
+    @room.locations.joins(:users).to_json
   end
 end
