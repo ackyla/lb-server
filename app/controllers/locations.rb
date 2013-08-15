faaae1667b89f3f12aeca8560706e6e4a6821931
phelrine <phelrine@gmail.com>
2013-08-15 16:09:27 +0900

    ロケーション

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -1,6 +1,7 @@
 Server::App.controllers :locations do
   post :create, :provides => :json do
     login(params)
+    error_message("300", "Room is not active") unless @user.room.active
     loc = Location.new(:latitude => params[:latitude], :longitude => params[:longitude]){|l|
       l.user = @user
       l.room = @user.room
@@ -8,9 +9,12 @@ Server::App.controllers :locations do
     loc.save
     loc.to_json
   end
-  
+
   get :list, :provides => :json do
     find_room(params)
-    @room.locations.to_json(:include => [:user])
+    {
+      members: @room.users,
+      locations: @room.locations
+    }.to_json
   end
 end
