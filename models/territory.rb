50d9397571c0b55a3393ae01c070a2393f4617af
phelrine <phelrine@gmail.com>
2013-12-28 18:36:51 +0900

    km -> m

--- models/territory.rb
+++ models/territory.rb
@@ -23,7 +23,7 @@ class Territory < ActiveRecord::Base
     x2 = loc.longitude * Math::PI / 180
     earth_r = 6378140
     deg = Math::sin(y1) * Math::sin(y2) + Math::cos(y1) * Math::cos(y2) * Math::cos(x2 - x1)
-    distance = earth_r * (Math::atan(-deg / Math::sqrt(-deg * deg + 1)) + Math::PI / 2) / 1000
+    distance = earth_r * (Math::atan(-deg / Math::sqrt(-deg * deg + 1)) + Math::PI / 2)
     return distance
   end
 end
